75d8477b6320b786c3ce7b8ac821b1cf755d8a08
mustafaColak0 <25640140050@mersin.edu.tr>
2025-12-12 01:44:56 +0300

    Add files via upload

---
+++ ARAC_KİRALAMA_SİSTEMİ/Arac_Kiralama_Sistemi/ARAC_KİRALAMA_SİSTEMİ/Program.cs
@@ -0,0 +1,464 @@
+﻿
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+
+namespace ARAC_KİRALAMA_SİSTEMİ
+{
+
+
+    class Rezervasyon
+    {
+        public string? MusteriAdi { get; set; } = "";
+        public string? AracPlaka { get; set; } = "";
+        public DateTime Baslangic;
+        public DateTime Bitis;
+        public double ToplamUcret;
+    }
+
+    class Arac
+    {
+        public string marka;
+        public string model;
+        public int yil;
+        public string renk;
+        public double gunlukUcret;
+        public string plaka { get; set; }
+
+        public Arac(string marka, string model, string plaka, int yil, string renk, double gunlukUcret)
+        {
+            this.marka = marka;
+            this.model = model;
+            this.plaka = plaka;
+            this.yil = yil;
+            this.renk = renk;
+            this.gunlukUcret = gunlukUcret;
+        }
+    }
+
+    class Program
+    {
+        static string dosyaYolu = "rezervasyonlar.txt";
+        static List<Arac> araclar = new List<Arac>()
+    {
+        new Arac ("Toyota", "Corolla","APT7641", 2020, "Beyaz", 1500),
+        new Arac("Honda", "Civic", "34SRT21",2019, "Siyah", 3500),
+        new Arac("Ford", "Focus","34STU07" ,2021, "Kirmizi", 2500),
+        new Arac("Range" , "Rover","34ABC10", 2022, "Mavi", 6000),
+        new Arac ( "BMW" , "320i","01ADN01",2022,"Mavi" , 5500 ),
+        new Arac ( "Audi" , "A6","34IZM05",2022,"Siyah" , 4000 ),
+        new Arac ( "Mercedes" , "AMG", "46RSP14",2022,"Gri" , 5000 ),
+    };
+
+        static List<Rezervasyon> rezervasyonlar = new List<Rezervasyon>();
+
+        static void Main(string[] args)
+        {
+
+            VerileriYukle();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("====Araç Kiralama Sistemi===");
+                Console.WriteLine("1.Müsait araçları listele");
+                Console.WriteLine("2.Yeni Rezervasyon yap");
+                Console.WriteLine("3.Rezervasyon iptal et");
+                Console.WriteLine("(Rapor):4.Toplam Gelir");
+                Console.WriteLine("(Rapor):5.Müşteri Rezervasyonları");
+                Console.WriteLine("(Rapor):6.En Çok kiralanan araç");
+                Console.WriteLine("7.Tüm Rezervasyonları Sıfırla");
+                Console.WriteLine("(0. Cikis)");
+                Console.Write("Seciminizi yapin: ");
+
+                string secim = Console.ReadLine() ?? "";
+                switch (secim)
+                {
+                    case "1":
+                        MusaitAraclariListeleEkrani();
+                        break;
+                    case "2":
+                        RezervasyonYapEkrani();
+                        break;
+                    case "3":
+                        RezervasyonİptalEtEkrani();
+                        break;
+                    case "4":
+
+                        Console.WriteLine($"\n Toplam Gelir: {ToplamGelir()} TL");
+                        Console.ReadKey();
+                        break;
+                    case "5":
+                        MusteriRezervasyonlariEkrani();
+                        break;
+                    case "6":
+                        Console.WriteLine($"\n En Popüler Araç: {EnCokKiralananArac()}");
+                        Bekle();
+                        break;
+
+                    case "7":
+                        TumRezervasyonlariSifirla();
+                         break;
+                    case "0":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Gecersiz secim. Tekrar deneyin.");
+                        
+                        Bekle(); break;
+                }
+            }
+        }
+        static void Bekle()
+        {
+            Console.WriteLine("\nDevam etmek için bir tuşa basın...");
+            Console.ReadKey();
+        }
+        static void TumRezervasyonlariSifirla()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\n DİKKAT: TÜM VERİLERİ SİLMEK ÜZERESİNİZ! ");
+            Console.ResetColor();
+
+            Console.WriteLine("Kayıtlı tüm rezervasyonlar kalıcı olarak silinecek.");
+            Console.Write("Onaylıyor musunuz? (Evet için 'E' yazın): ");
+
+            string onay = Console.ReadLine() ?? "";
+
+            if (onay.ToUpper() == "E")
+            {
+               
+                rezervasyonlar.Clear();
+
+                
+                if (File.Exists(dosyaYolu))
+                {
+                    File.Delete(dosyaYolu); 
+                }
+
+                Console.WriteLine("\n Başarılı: Tüm rezervasyonlar ve dosya sıfırlandı.");
+            }
+            else
+            {
+                Console.WriteLine("\n İşlem iptal edildi. Veriler silinmedi.");
+            }
+            Bekle();
+        }
+
+
+
+        static void VerileriKaydet()
+        {
+            try
+            {
+
+
+                using (StreamWriter sw = new StreamWriter(dosyaYolu))
+                {
+                    foreach (var rezervasyon in rezervasyonlar)
+                    {
+
+                        string kayit = $"{rezervasyon.AracPlaka}|{rezervasyon.MusteriAdi}|{rezervasyon.Baslangic}|{rezervasyon.Bitis}|{rezervasyon.ToplamUcret}";
+                        sw.WriteLine(kayit);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Kayıt hatası: {ex.Message}");
+            }
+        }
+
+        static void VerileriYukle()
+        {
+
+            try
+
+            {
+                if (!File.Exists(dosyaYolu)) return;
+                string[] satirlar = File.ReadAllLines(dosyaYolu);
+
+                foreach (string satir in satirlar)
+                {
+
+                    {
+                        string[] secenek = satir.Split('|');
+
+                        if (secenek.Length == 5)
+                        {
+                            Rezervasyon eskiRezervasyon = new Rezervasyon
+                            {
+                                AracPlaka = secenek[0],
+                                MusteriAdi = secenek[1],
+                                Baslangic = DateTime.Parse(secenek[2]),
+                                Bitis = DateTime.Parse(secenek[3]),
+                                ToplamUcret = double.Parse(secenek[4])
+                            };
+                            rezervasyonlar.Add(eskiRezervasyon);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Yükleme hatası: {ex.Message}");
+                Bekle();
+            }
+        }
+
+        static bool AracMusaitMi(string aracPlaka, DateTime baslangic, DateTime bitis)
+        {
+            for (int i = 0; i < rezervasyonlar.Count; i++)
+            {
+                Rezervasyon rezervasyon = rezervasyonlar[i];
+
+                if (rezervasyon.AracPlaka == aracPlaka)
+                {
+
+                    if (baslangic < rezervasyon.Bitis && bitis > rezervasyon.Baslangic)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        static double AracGunlukUcretGetir(string aracPlaka)
+        {
+            foreach (var arac in araclar)
+            {
+                if (arac.plaka.Trim() == aracPlaka.Trim())
+                    return arac.gunlukUcret;
+            }
+            return 0;
+        }
+
+        static double RezervasyonUcretHesapla(string aracPlaka, DateTime baslangic, DateTime bitis)
+        {
+            double gunlukUcret = AracGunlukUcretGetir(aracPlaka);
+            int kiralamaGunu = (bitis - baslangic).Days;
+
+            if (kiralamaGunu <= 0) return 0;
+            return gunlukUcret * kiralamaGunu;
+        }
+
+
+
+        static void RezervasyonYap(string musteri, string plaka, DateTime baslangic, DateTime bitis)
+        {
+            if (AracMusaitMi(plaka, baslangic, bitis))
+            {
+                double toplamUcret = RezervasyonUcretHesapla(plaka, baslangic, bitis);
+
+                Rezervasyon yeniRezervasyon = new Rezervasyon();
+                yeniRezervasyon.MusteriAdi = musteri;
+                yeniRezervasyon.AracPlaka = plaka;
+                yeniRezervasyon.Baslangic = baslangic;
+                yeniRezervasyon.Bitis = bitis;
+                yeniRezervasyon.ToplamUcret = toplamUcret;
+
+
+                rezervasyonlar.Add(yeniRezervasyon);
+                VerileriKaydet();
+
+                Console.WriteLine($"\n Rezervasyon başarılı! Toplam Ücret: {toplamUcret} TL");
+            }
+            else
+            {
+                Console.WriteLine("\n Seçilen araç bu tarihler arasında müsait değildir.");
+            }
+        }
+
+        static void RezervasyonIptalEt(string aracPlaka, string musteriAdi)
+        {
+            Rezervasyon? rezervasyonToRemove = null;
+
+            foreach (var rezervasyon in rezervasyonlar)
+            {
+                if (rezervasyon.MusteriAdi == musteriAdi && rezervasyon.AracPlaka == aracPlaka)
+                {
+                    rezervasyonToRemove = rezervasyon;
+                    break;
+                }
+            }
+            if (rezervasyonToRemove != null)
+            {
+                rezervasyonlar.Remove(rezervasyonToRemove);
+                VerileriKaydet();
+
+                Console.WriteLine("\n Rezervasyon iptal edildi.");
+            }
+            else
+            {
+                Console.WriteLine("\n Rezervasyon bulunamadı.");
+            }
+        }
+
+        static double ToplamGelir()
+        {
+            double toplam = 0;
+
+            foreach (var rezervasyon in rezervasyonlar)
+            {
+                toplam += rezervasyon.ToplamUcret;
+            }
+            return toplam;
+        }
+
+        static List<string> MusteriRezervasyonlari(string musteriAdi)
+        {
+            List<string> liste = new List<string>();
+
+            foreach (var rezervasyon in rezervasyonlar)
+            {
+                if (rezervasyon.MusteriAdi == musteriAdi)
+                {
+                    liste.Add($"- Araç Plakası: {rezervasyon.AracPlaka}, Başlangıç: {rezervasyon.Baslangic.ToShortDateString()}, Bitiş: {rezervasyon.Bitis.ToShortDateString()}, Toplam Ücret: {rezervasyon.ToplamUcret} TL");
+                }
+            }
+            return liste;
+        }
+
+        static string EnCokKiralananArac()
+        {
+            Dictionary<string, int> aracKiralamaSayilari = new Dictionary<string, int>();
+
+            foreach (var rezervasyon in rezervasyonlar)
+            {
+
+                if (string.IsNullOrEmpty(rezervasyon.AracPlaka)) continue;
+
+                if (aracKiralamaSayilari.ContainsKey(rezervasyon.AracPlaka))
+                {
+                    aracKiralamaSayilari[rezervasyon.AracPlaka]++;
+                }
+                else
+                {
+                    aracKiralamaSayilari[rezervasyon.AracPlaka] = 1;
+                }
+
+            }
+
+            string? enCokKiralananArac = null;
+            int maxKiralama = 0;
+
+            foreach (var arac in aracKiralamaSayilari)
+            {
+                if (arac.Value > maxKiralama)
+                {
+                    maxKiralama = arac.Value;
+                    enCokKiralananArac = arac.Key;
+                }
+            }
+            return enCokKiralananArac ?? "Henüz araç kiralanmamış.";
+        }
+
+
+
+        static void MusaitAraclariListeleEkrani()
+        {
+            Console.WriteLine("\n ŞU AN MÜSAİT OLAN ARAÇLAR (Anlık Durum):");
+            Console.WriteLine("------------------------------------------------");
+
+            bool aracVar = false;
+            DateTime suan = DateTime.Now;
+
+
+            foreach (var arac in araclar)
+            {
+
+                bool musait = AracMusaitMi(arac.plaka, suan, suan);
+
+                if (musait)
+                {
+                    Console.WriteLine($"-  {arac.marka} {arac.model} [{arac.plaka}] ({arac.yil})  {arac.renk}  {arac.gunlukUcret} TL");
+                    aracVar = true;
+                }
+            }
+
+            if (!aracVar)
+            {
+                Console.WriteLine(" Şu anda garajda boş araç yok, hepsi kirada.");
+            }
+            Console.WriteLine("\n Ana Menüye dönmek için bir tuşa basın...");
+
+            Console.ReadKey();
+        }
+
+        static void RezervasyonYapEkrani()
+        {
+            try
+            {
+                Console.Write("\nMüşteri Adı: ");
+                string musteriAdi = Console.ReadLine() ?? "";
+
+                Console.Write("Araç Plakası: ");
+                string aracPlaka = Console.ReadLine() ?? "";
+
+                if (AracGunlukUcretGetir(aracPlaka) == 0)
+                {
+                    Console.WriteLine(" HATA: Bu plakaya sahip bir araç bulunamadı.");
+                    return; 
+                }
+
+                Console.Write("Kiralama Başlangıç Tarihi (yyyy-MM-dd): ");
+                DateTime baslangic = DateTime.Parse(Console.ReadLine() ?? "");
+
+                Console.Write("Kiralama Bitiş Tarihi (yyyy-MM-dd): ");
+                DateTime bitis = DateTime.Parse(Console.ReadLine() ?? "");
+
+                RezervasyonYap(musteriAdi, aracPlaka, baslangic, bitis);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Hatalı tarih girdin! Lütfen formatı düzgün yaz (Örn: 2023-12-01).");
+            }
+            Console.ReadKey();
+        }
+
+        static void RezervasyonİptalEtEkrani()
+        {
+            Console.Write("\n Müşteri Adı: ");
+            string musteriAdi = Console.ReadLine() ?? "";
+
+            Console.Write("İptal edilecek aracın plakası: ");
+            string aracPlaka = Console.ReadLine() ?? "";
+
+            RezervasyonIptalEt(aracPlaka, musteriAdi);
+            Console.ReadKey();
+        }
+
+        static void MusteriRezervasyonlariEkrani()
+        {
+
+            Console.Write("\n Müşteri Adı: ");
+            string musteriAdi = Console.ReadLine() ?? "";
+
+            var liste = MusteriRezervasyonlari(musteriAdi);
+
+            Console.WriteLine($"\n--- {musteriAdi} İçin Kayıtlar ---");
+
+            if (liste.Count == 0) Console.WriteLine("Kayıt bulunamadı.");
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
+
